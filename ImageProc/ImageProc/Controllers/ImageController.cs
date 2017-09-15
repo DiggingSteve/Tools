a6df73de17f4e9a2adfb31fc0dfab1db9305ae90
Steve <doublefly@live.cn>
2017-09-15 16:35:34 +0800

    init

---
+++ ImageProc/ImageProc/Controllers/ImageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ImageProc.Controllers
+{
+    public class ImageController : ApiController
+    {
+        public IHttpActionResult GetImage(int w, int h)
+        {
+            using (var img = new ImageProcessor.ImageFactory())
+            {
+                string virtualPath = ConfigurationManager.AppSettings["imageDoc"] + "/timg.jpg";
+                string thumbnailPath = ConfigurationManager.AppSettings["imageThumbsDoc"] + "/timg.jpg";
+                img.Load(FileUtil.GetMapPath(virtualPath));
+                if (w > 0)
+                {
+                    var size = new System.Drawing.Size() { Width = w, Height = h };
+                    img.Resize(new ImageProcessor.Imaging.ResizeLayer(size,
+                        resizeMode: ImageProcessor.Imaging.ResizeMode.Max,
+                        anchorPosition: ImageProcessor.Imaging.AnchorPosition.Center));
+                }
+                img.Alpha(50);
+                img.Quality(50);
+                img.Save(string.Format(FileUtil.GetMapPath(thumbnailPath)));
+                string host = HttpContext.Current.Request.Url.Host;
+                string port = HttpContext.Current.Request.Url.Port.ToString();
+                string redirectPath = "http://" + host + ":" + port + thumbnailPath.Replace("~", "");
+                return Redirect(redirectPath);
+
+            }
+        }
+    }
+}
---
+++ ImageProc/ImageProc/Extend.cs
@@ -0,0 +1,66 @@
+﻿
+
+namespace System
+{
+    public static class Extend
+    {
+
+        /// <summary>
+        /// 转换为INT类型  失败为0
+        /// </summary>
+        /// <param name="_this"></param>
+        /// <param name="def">转换失败的默认值</param>
+        /// <returns></returns>
+        public static int ToInt(this object _this, int def = 0)
+        {
+            return _this.ToNumber<int>(def);
+        }
+
+        /// <summary>
+        /// 转换为数值类型
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_this"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static T ToNumber<T>(this object _this, T def = default(T)) where T : struct
+        {
+            if (_this == null || _this == DBNull.Value)
+            {
+                return def;
+            }
+            T result = def;
+            if (_this is String)
+            {
+                try
+                {
+                    var _ret = Convert.ChangeType(_this, typeof(T));
+                    return (T)_ret;
+                }
+                catch (Exception ex)
+                {
+                    return def;
+                }
+            }
+            else
+            {
+                if (_this is Boolean)
+                {
+                    return (T)((bool)_this ? (object)1 : (object)0);
+                }
+                return (T)_this;
+            }
+        }
+
+        public static string TrimStart(this string _this, string trimString)
+        {
+            string result = _this;
+            while (result.StartsWith(trimString))
+            {
+                result = result.Substring(trimString.Length);
+            }
+
+            return result;
+        }
+    }
+}
